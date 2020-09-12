07d821b2fa5905aa794a038d0ef05cd601268327
Vineet <vsmail2018@gmail.com>
2020-09-12 20:03:19 +0530

    Add PasswordEncoder to security configuration

--- spring-security-jwt/src/main/java/com/learning/spring/security/SecurityConfiguration.java
+++ spring-security-jwt/src/main/java/com/learning/spring/security/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.learning.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.learning.spring.service.MyUserDetailsService;
 
@@ -17,5 +20,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception 
 	{
 		auth.userDetailsService(myUserDetailsService);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
