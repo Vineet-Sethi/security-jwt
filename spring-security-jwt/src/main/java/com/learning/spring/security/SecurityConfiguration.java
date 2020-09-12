757fcb2da57cf94c7ac059ea3c8f103415c2fbc5
Vineet <vsmail2018@gmail.com>
2020-09-13 01:24:28 +0530

    configure Authentication urls and add AuthManagerBean

--- spring-security-jwt/src/main/java/com/learning/spring/security/SecurityConfiguration.java
+++ spring-security-jwt/src/main/java/com/learning/spring/security/SecurityConfiguration.java
@@ -2,7 +2,9 @@ package com.learning.spring.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -20,6 +22,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception 
 	{
 		auth.userDetailsService(myUserDetailsService);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		     .authorizeRequests().antMatchers("/authenticate").permitAll()
+		     .anyRequest().authenticated();
+	}
+	
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception 
+	{
+		return super.authenticationManager();
 	}
 	
 	@Bean
