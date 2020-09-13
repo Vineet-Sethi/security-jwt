f29d111d0f6a3c682c9fab27789903a962c26114
Vineet <vsmail2018@gmail.com>
2020-09-13 13:18:17 +0530

    Add security configiration for filter

--- spring-security-jwt/src/main/java/com/learning/spring/security/SecurityConfiguration.java
+++ spring-security-jwt/src/main/java/com/learning/spring/security/SecurityConfiguration.java
@@ -35,7 +35,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 		     .authorizeRequests().antMatchers("/authenticate").permitAll()
-		     .anyRequest().authenticated();
+		     .anyRequest().authenticated()
+		     .and().sessionManagement()
+		     .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	@Override
